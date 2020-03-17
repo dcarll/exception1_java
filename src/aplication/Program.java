34f572d34b90c415975c3249f2799b6fb949006c
dcarll <diego_carlos110@hotmail.com>
2020-03-16 23:58:27 -0300

    Solução boa

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -6,51 +6,43 @@ import java.util.Date;
 import java.util.Scanner;
 
 import model.entities.Reservation;
+import nodel.exception.DomainException;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args){
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
-		System.out.print("Room number: ");
-		int number = scan.nextInt();
-		System.out.println("Check-In date (dd/MM/yyyy): ");
-		Date checkIn = sdf.parse(scan.next());
-		System.out.println("Check-Out date (dd/MM/yyyy): " );
-		Date checkOut = sdf.parse(scan.next());
-		
-		if(!checkOut.after(checkIn)) {
-			System.out.println("Error in reservation: check-out date must be after check-in date");
-			
-		}else {
-			Reservation reservation =new Reservation(number, checkIn, checkOut);
+		try {
+			System.out.print("Room number: ");
+			int number = scan.nextInt();
+			System.out.println("Check-In date (dd/MM/yyyy): ");
+			Date checkIn = sdf.parse(scan.next());
+			System.out.println("Check-Out date (dd/MM/yyyy): ");
+			Date checkOut = sdf.parse(scan.next());
+	
+			Reservation reservation = new Reservation(number, checkIn, checkOut);
 			System.out.println("reservation: " + reservation);
-			
+	
 			System.out.println();
 			System.out.println("Enter data to update the reservation: ");
 			System.out.println("Check-In date (dd/MM/yyyy): ");
 			checkIn = sdf.parse(scan.next());
-			System.out.println("Check-Out date (dd/MM/yyyy): " );
+			System.out.println("Check-Out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(scan.next());
+	
+			reservation.upDateDates(checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
 			
-			
-			Date now = new Date();
-			
-			if(checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: update data must be after for chack-in and check-out");
-				
-			}else if(!checkOut.after(checkIn)){
-				System.out.println("Error in reservation: check-out date must be after check-in date");
-			}
-			
-			else {
-				reservation.upDateDates(checkIn, checkOut);
-				System.out.println("reservation: " + reservation);
-			}
-			
+		}catch (ParseException e) {
+			System.out.println("Vc digitou uma data como formato inválido");
+		}catch (DomainException e) {
+			System.out.println("Erro na reserva" + e.getMessage());
+		}catch (RuntimeException e) {
+			System.out.println("Error inesperado");
 		}
+
 	}
 
 }
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,18 +1,24 @@
 package model.entities;
 
 import java.text.SimpleDateFormat;
-import java.time.Duration;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import nodel.exception.DomainException;
+
 public class Reservation {
+	
 	
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
 	
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
-	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) throws DomainException {
+		
+		if(!checkOut.after(checkIn)){
+			throw new DomainException("check-out date mus tbe after check-in date");
+		}
 
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
@@ -36,9 +42,19 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	public void upDateDates(Date checkIn, Date chaeckOut) {
+	public void upDateDates(Date checkIn, Date checkOut) throws DomainException{
+		
+		Date now = new Date();
+		
+		if(checkIn.before(now) || checkOut.before(now)) {
+			throw new DomainException("Reservatio dates for update must be before future update");
+			
+		}if(!checkOut.after(checkIn)){
+			throw new DomainException("check-out date mus tbe after check-in date");
+		}
 		this.checkIn = checkIn;
-		this.checkOut = chaeckOut;
+		this.checkOut = checkOut;
+		
 	}
 	
 	@Override
